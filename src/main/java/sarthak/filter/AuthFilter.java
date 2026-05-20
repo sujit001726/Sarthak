d9d4c5fd4f122c29486d98cd15cf12cecc827944
aakriti01 <aakritinepal224@gmail.com>
2026-05-20 19:39:34 +0545

    Create skeleton for AuthFilter

---
+++ src/main/java/sarthak/filter/AuthFilter.java
@@ -0,0 +1,24 @@
+﻿package sarthak.filter;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = {"/admin", "/pages/*"})
+public class AuthFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // Skeleton filter: pass through requests for now
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {}
+}
